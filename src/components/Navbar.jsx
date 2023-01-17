3542d5e5df94091ba22251da599ce23e2b08adcf
Reginald Wong <reginald_wong@outlook.com>
2023-01-17 16:23:51 -0500

    update icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
     return (
         <nav>
-            <img src="./images/smile-beam.png" alt="smile face" className="nav--logo" />
+            <i class="fi fi-rr-biking-mountain"></i>
             <h3 className="nav--text">Reginald Wong</h3>
             <h3 className="nav--title">Software Developer - Full Stack</h3>
         </nav>
