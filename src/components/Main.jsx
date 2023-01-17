9e95c235bf2113ec90e1f0cf3bc7999cad3c3dd7
Reginald Wong <reginald_wong@outlook.com>
2023-01-16 23:53:07 -0500

    first commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Main = () => <h1>Main here</h1>;
+
+export default Main;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav>
+            <img src="./images/smile-beam.png" alt="smile face" className="nav--logo" />
+            <h3 className="nav--text">Reginald Wong</h3>
+            <h4 className="nav--title">Software Developer - Full Stack</h4>
+        </nav>
+    )
+}
+
+export default Navbar;
