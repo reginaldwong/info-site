973ee9676d58c806493642603fd47c83215232bc
Reginald Wong <reginald_wong@outlook.com>
2023-01-18 16:02:16 -0500

    add contact links to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,11 @@ const Footer = () => {
     return (
         <footer>
             <h1 className="footer--title">Contact</h1>
+            <p>I am currently available for full time opportunities. However, if you have any other requests or questions, do not hesitate to get in touch!</p>
                 <ul className="footer--links">
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
-                    <li>4</li>
-                    <li>5</li>
+                    <li><a href="https://github.com/reginaldwong" target="_blank">Github</a></li>
+                    <li><a href="https://linkedin.com/in/reginaldwong" target="_blank">LinkedIn</a></li>
+                    <li><a href="mailto:reginaldwongdev@gmail.com">Email</a></li>
                 </ul>
         </footer>
     )};
