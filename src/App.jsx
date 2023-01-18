1462296b7fe78d9229ac49abb73f98525c5fa4ed
Reginald Wong <reginald_wong@outlook.com>
2023-01-18 09:27:06 -0500

    add footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Main from "./components/Main"
 import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <div className="container">
     <Navbar />
     <Main />
+    <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer>
+            <h1 className="footer--title">Contact</h1>
+                <ul className="footer--links">
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                    <li>4</li>
+                    <li>5</li>
+                </ul>
+        </footer>
+    )};
+
+
+export default Footer;
