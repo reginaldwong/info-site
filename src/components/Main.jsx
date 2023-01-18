e6da86104bc80bd1c424398a3431621017e05f09
Reginald Wong <reginald_wong@outlook.com>
2023-01-17 20:57:12 -0500

    added anchor tags to nav text and about me

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,8 +7,8 @@ const Main = () => {
                 <ul className="main--facts">
                     <li>Toronto based full stack developer, specializing in the MERN stack</li>
                     <li>Strong suit for design, functionality, and user experience</li>
-                    <li>Currently working as a freelance frontend developer for nonprofit sports league</li>
                     <li>Interested in building software that improves the lives of those around me</li>
+                    <li>Currently working as a freelance frontend developer for a <a href="https://murkville.wordpress.com/" target="_blank">nonprofit sports league</a></li>
                     <li>Available for full time opportunities!</li>
                 </ul>
         </main>
