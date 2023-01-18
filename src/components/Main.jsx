a59eef51057ecde930c2531b0c75742934afd8f5
Reginald Wong <reginald_wong@outlook.com>
2023-01-17 20:52:19 -0500

    style main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
-const Main = () => <h1>Main here</h1>;
+const Main = () => {
+    return (
+        <main>
+            <h1 className="main--title">About Me!</h1>
+                <ul className="main--facts">
+                    <li>Toronto based full stack developer, specializing in the MERN stack</li>
+                    <li>Strong suit for design, functionality, and user experience</li>
+                    <li>Currently working as a freelance frontend developer for nonprofit sports league</li>
+                    <li>Interested in building software that improves the lives of those around me</li>
+                    <li>Available for full time opportunities!</li>
+                </ul>
+        </main>
+    )};
+
 
 export default Main;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,8 @@ const Navbar = () => {
     return (
         <nav>
             <i class="fi fi-rr-biking-mountain"></i>
-            <h3 className="nav--text">Reginald Wong</h3>
-            <h3 className="nav--title">Software Developer - Full Stack</h3>
+            <h3 className="nav--text"><a href="https://reginaldwong.com" target="_blank">Reginald Wong</a></h3>
+            <h3 className="nav--title">Full Stack Software Developer</h3>
         </nav>
     )
 }
