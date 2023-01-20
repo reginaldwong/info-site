9208ae0d8902eaa756cf8ddce593abcaa2b3bd23
Reginald Wong <reginald_wong@outlook.com>
2023-01-19 23:51:12 -0500

    add hover effect to footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer>
             <h1 className="footer--title">Contact</h1>
-            <p>I am currently available for full time opportunities. However, if you have any other requests or questions, do not hesitate to get in touch!</p>
+            <p className="footer--text">I am currently available for full time opportunities. However, if you have any other requests or questions, do not hesitate to get in touch!</p>
                 <ul className="footer--links">
                     <li><a href="https://github.com/reginaldwong" target="_blank">Github</a></li>
                     <li><a href="https://linkedin.com/in/reginaldwong" target="_blank">LinkedIn</a></li>
