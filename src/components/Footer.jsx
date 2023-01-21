925c26e099b88a2de634277ab5e3bbe82ce3a341
Reginald Wong <reginald_wong@outlook.com>
2023-01-20 22:18:52 -0500

    fix sticky footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,13 @@ const Footer = () => {
     return (
         <footer>
             <h1 className="footer--title">Contact</h1>
-            <p className="footer--text">I am currently available for full time opportunities. However, if you have any other requests or questions, do not hesitate to get in touch!</p>
-                <ul className="footer--links">
+            <ul className="footer--links">
                     <li><a href="https://github.com/reginaldwong" target="_blank">Github</a></li>
                     <li><a href="https://linkedin.com/in/reginaldwong" target="_blank">LinkedIn</a></li>
                     <li><a href="mailto:reginaldwongdev@gmail.com">Email</a></li>
                 </ul>
+            <p className="footer--text">I am currently available for full time opportunities. However, if you have any other requests or questions, do not hesitate to get in touch!</p>
+              
         </footer>
     )};
 
